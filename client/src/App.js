fcbe743a1740d444b5a4abc723d5d1e073d50329
saiffmirza <saiff.mirza@gmail.com>
2021-06-09 20:01:14 -0400

    important changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,18 @@ import "./App.css";
 //components
 
 import InputTodo from "./components/InputTodo";
-//import EditTodo from "./components/EditTodo";
+//import EditTodo from "./components/EditTodo"; !referenced in InputTodo.js
 import ListTodo from "./components/ListTodo";
 
 function App() {
-  return (
-    <div className="container">
-      <Fragment>
-        <InputTodo />
-        <ListTodo />
-      </Fragment>
-    </div>
-  );
+	return (
+		<div className="container">
+			<Fragment>
+				<InputTodo />
+				<ListTodo />
+			</Fragment>
+		</div>
+	);
 }
 
 export default App;
